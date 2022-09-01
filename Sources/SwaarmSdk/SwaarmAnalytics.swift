e3246764af98110e45b4e7dcbed9eb47ced024a6
Wolfgang Pfnür <wolfgang.pfnuer@swaarm.com>
2022-09-01 13:07:32 +0200

    @objc

--- Sources/SwaarmSdk/SwaarmAnalytics.swift
+++ Sources/SwaarmSdk/SwaarmAnalytics.swift
@@ -4,14 +4,16 @@ import os.log
 import UIKit
 import WebKit
 
-public enum SwaarmAnalytics {
+@objc(SwaarmAnalytics)
+public class SwaarmAnalytics: NSObject
+ {
     private static var eventRepository: EventRepository?
     private static var publisher: EventPublisher?
     private static var isInitialized: Bool = false
     private static var urlSession: URLSession = .shared
     private static var apiQueue: DispatchQueue = .init(label: "swaarm-api")
 
-    public static func configure(config: SwaarmConfig? = nil, token: String? = nil, host: String? = nil,
+    @objc public static func configure(config: SwaarmConfig? = nil, token: String? = nil, host: String? = nil,
                                  batchSize: Int = 50, flushFrequency: Int = 10, maxSize: Int = 500,
                                  debug: Bool = false)
     {
@@ -76,7 +78,7 @@ public enum SwaarmAnalytics {
         }
     }
 
-    public static func event(typeId: String? = nil, aggregatedValue: Double = 0.0, customValue: String = "", revenue: Double = 0.0) {
+    @objc public static func event(typeId: String? = nil, aggregatedValue: Double = 0.0, customValue: String = "", revenue: Double = 0.0) {
         if isInitialized == false {
             Logger.debug("Tracker is not initialized")
             return
@@ -86,21 +88,17 @@ public enum SwaarmAnalytics {
         Logger.debug("received event with typeId \(typeId as String?) aggregatedValue \(aggregatedValue) customValue \(customValue) revenue \(revenue)")
     }
 
-    public static func configure(appToken: String, eventIngressHostname: String) {
-        configure(config: SwaarmConfig(appToken: appToken, eventIngressHostname: eventIngressHostname))
-    }
-
-    public static func disableTracking() {
+    @objc public static func disableTracking() {
         publisher!.stop()
         Logger.debug("Tracking disabled")
     }
 
-    public static func enableTracking() {
+    @objc public static func enableTracking() {
         publisher!.start()
         Logger.debug("Tracking resumed")
     }
 
-    public static func debug(enable: Bool) {
+    @objc public static func debug(enable: Bool) {
         Logger.setIsEnabled(enabled: enable)
     }
 
--- Tests/SwaarmSdkTests/SwaarmSdkTests.swift
+++ Tests/SwaarmSdkTests/SwaarmSdkTests.swift
@@ -6,6 +6,5 @@ final class SwaarmSdkTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwaarmSdk().text, "Hello, World!")
     }
 }
